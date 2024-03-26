7394bcff66993408d9febf0ee391f19d3601591e
Shairah Nicole Manzon <shairahmanzon03@gmail.com>
2024-03-26 20:30:15 +0800

    Update Login.aspx.cs

--- Dans_Cafe/Login.aspx.cs
+++ Dans_Cafe/Login.aspx.cs
@@ -1,4 +1,3 @@
-﻿using Dans_Cafe.App.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Dans_Cafe
 {
-    public partial class Login : System.Web.UI.Page
+    public partial class Login : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,32 +17,33 @@ namespace Dans_Cafe
         protected void loginBtn_Click(object sender, EventArgs e)
         {
             var userRepo = new RepositoryUsers();
+            var username = userNtxtbox.Text;
+            var password = passtxtbox.Text;
 
-            var currentUser = userRepo.LogIn(userNtxtbox.Text);
-
-            if (currentUser != null && currentUser.Any())
+            try
             {
-                foreach (var user in currentUser)
+                var currentUser = userRepo.LogIn(username, password);
+
+                if (currentUser != null)
                 {
-                    if (user.Username != null)
-                    {
-                        if (user.Password == passtxtbox.Text)
-                        {
-                            Response.Redirect($"Home.aspx?username={userNtxtbox.Text}");
-                        }
-                        else
-                        {
-                            string wrongPass = "Incorrect Password";
-                            Response.Write($"<script>alert('{wrongPass}')</script>");
-                        }
-                    }
-                    else
-                    {
-                        string notUser = "User does not exist.";
-                        Response.Write($"<script>alert('{notUser}')</script>");
-                    }
+                    Response.Redirect($"Home.aspx?username={username}");
+                }
+                else
+                {
+                    string errorMessage = "Incorrect username or password.";
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('{errorMessage}');", true);
                 }
             }
+            catch (Exception ex)
+            {
+                string errorMessage = "An error occurred while logging in.";
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('{errorMessage}');", true);
+                // Log the exception
+                // LogException(ex);
+            }
+        }
+    }
+}
 
             //var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\Aaliyah Gutierrez\\source\\repos\\Login\\Login\\App_Data\\Users.mdf\";Integrated Security=True";
 
